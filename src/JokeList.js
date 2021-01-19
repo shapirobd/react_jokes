d8a7c6273a99ba3848f3c7e3e997836c5eb7486f
Brian Shapiro <briandavidshapiro@gmail.com>
2021-01-19 18:29:03 -0500

    completed everything before further study, as well as loading visual

--- src/JokeList.js
+++ src/JokeList.js
@@ -3,72 +3,95 @@ import axios from "axios";
 import Joke from "./Joke";
 import "./JokeList.css";
 
-function JokeList({ numJokesToGet = 10 }) {
-  const [jokes, setJokes] = useState([]);
+class JokeList extends React.Component {
+	static defaultProps = { numJokesToGet: 10 };
+	constructor(props) {
+		super(props);
+		this.state = {
+			jokes: [],
+		};
+		this.getJokes = this.getJokes.bind(this);
+		this.generateNewJokes = this.generateNewJokes.bind(this);
+		this.vote = this.vote.bind(this);
+	}
 
-  /* get jokes if there are no jokes */
+	componentDidMount() {
+		if (this.state.jokes.length === 0) this.getJokes();
+	}
+	componentDidUpdate() {
+		if (this.state.jokes.length === 0) this.getJokes();
+	}
+	/* get jokes if there are no jokes */
 
-  useEffect(function() {
-    async function getJokes() {
-      let j = [...jokes];
-      let seenJokes = new Set();
-      try {
-        while (j.length < numJokesToGet) {
-          let res = await axios.get("https://icanhazdadjoke.com", {
-            headers: { Accept: "application/json" }
-          });
-          let { status, ...jokeObj } = res.data;
-  
-          if (!seenJokes.has(jokeObj.id)) {
-            seenJokes.add(jokeObj.id);
-            j.push({ ...jokeObj, votes: 0 });
-          } else {
-            console.error("duplicate found!");
-          }
-        }
-        setJokes(j);
-      } catch (e) {
-        console.log(e);
-      }
-    }
+	async getJokes() {
+		let j = [...this.state.jokes];
+		let seenJokes = new Set();
+		try {
+			while (j.length < this.props.numJokesToGet) {
+				let res = await axios.get("https://icanhazdadjoke.com", {
+					headers: { Accept: "application/json" },
+				});
+				let { status, ...jokeObj } = res.data;
 
-    if (jokes.length === 0) getJokes();
-  }, [jokes, numJokesToGet]);
+				if (!seenJokes.has(jokeObj.id)) {
+					seenJokes.add(jokeObj.id);
+					j.push({ ...jokeObj, votes: 0 });
+				} else {
+					console.error("duplicate found!");
+				}
+			}
+			this.setState({ jokes: j });
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-  /* empty joke list and then call getJokes */
+	/* empty joke list and then call getJokes */
 
-  function generateNewJokes() {
-    setJokes([]);
-  }
+	generateNewJokes() {
+		this.setState({ jokes: [] });
+	}
 
-  /* change vote for this id by delta (+1 or -1) */
+	/* change vote for this id by delta (+1 or -1) */
 
-  function vote(id, delta) {
-    setJokes(allJokes =>
-      allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
-    );
-  }
+	vote(id, delta) {
+		const allJokes = this.state.jokes.map((j) =>
+			j.id === id ? { ...j, votes: j.votes + delta } : j
+		);
+		this.setState({ jokes: allJokes });
+	}
 
-  /* render: either loading spinner or list of sorted jokes. */
+	/* render: either loading spinner or list of sorted jokes. */
 
-  if (jokes.length) {
-    let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
-  
-    return (
-      <div className="JokeList">
-        <button className="JokeList-getmore" onClick={generateNewJokes}>
-          Get New Jokes
-        </button>
-  
-        {sortedJokes.map(j => (
-          <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={vote} />
-        ))}
-      </div>
-    );
-  }
+	render() {
+		if (this.state.jokes.length) {
+			let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes);
+			return (
+				<div className="JokeList">
+					<button className="JokeList-getmore" onClick={this.generateNewJokes}>
+						Get New Jokes
+					</button>
 
-  return null;
+					{sortedJokes.map((j) => (
+						<Joke
+							text={j.joke}
+							key={j.id}
+							id={j.id}
+							votes={j.votes}
+							vote={this.vote}
+						/>
+					))}
+				</div>
+			);
+		}
+		return (
+			<div>
+				<p>Loading...</p>
+			</div>
+		);
+	}
 
+	// return null;
 }
 
 export default JokeList;
